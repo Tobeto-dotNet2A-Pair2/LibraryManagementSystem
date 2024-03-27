16e2940f4eef137948cc481cf9301ce63b6e12ac
Zeynep Yaşar <zynpsema@gmail.com>
2024-03-27 15:51:42 +0300

    add configuration for all addresses data

--- librarymanagementsystem-backend/src/librarymanagementsystembackend/Persistence/EntityConfigurations/DistrictConfiguration.cs
+++ librarymanagementsystem-backend/src/librarymanagementsystembackend/Persistence/EntityConfigurations/DistrictConfiguration.cs
@@ -17,6 +17,9 @@ public class DistrictConfiguration : IEntityTypeConfiguration<District>
         builder.Property(d => d.UpdatedDate).HasColumnName("UpdatedDate");
         builder.Property(d => d.DeletedDate).HasColumnName("DeletedDate");
 
+        builder.HasOne(d => d.City);
+        builder.HasMany(d => d.Neighborhoods);
+
         builder.HasQueryFilter(d => !d.DeletedDate.HasValue);
     }
 }
