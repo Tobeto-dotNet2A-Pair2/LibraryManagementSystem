e39c60ded11cb3807836530a23a1b2b14d51b878
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-03-29 01:19:00 +0300

    add admin-layout

--- librarymanagementsystem-frontend/src/app/shared/layouts/admin-layout/admin-layout.component.spec.ts
+++ librarymanagementsystem-frontend/src/app/shared/layouts/admin-layout/admin-layout.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { RegisterpageComponent } from './registerpage.component';
+import { AdminLayoutComponent } from './admin-layout.component';
 
-describe('RegisterpageComponent', () => {
-  let component: RegisterpageComponent;
-  let fixture: ComponentFixture<RegisterpageComponent>;
+describe('AdminLayoutComponent', () => {
+  let component: AdminLayoutComponent;
+  let fixture: ComponentFixture<AdminLayoutComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RegisterpageComponent]
+      imports: [AdminLayoutComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(RegisterpageComponent);
+    fixture = TestBed.createComponent(AdminLayoutComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ librarymanagementsystem-frontend/src/app/shared/layouts/admin-layout/admin-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-layout.component.html',
+  styleUrl: './admin-layout.component.scss'
+})
+export class AdminLayoutComponent {
+
+}
