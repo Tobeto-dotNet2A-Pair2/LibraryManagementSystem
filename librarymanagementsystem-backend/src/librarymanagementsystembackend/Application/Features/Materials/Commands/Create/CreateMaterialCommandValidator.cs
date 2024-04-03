916ba2a6af64fef27e2372c139386d1141fa85cc
Zeynep Yaşar <zynpsema@gmail.com>
2024-04-03 16:52:20 +0300

    add material validation

--- librarymanagementsystem-backend/src/librarymanagementsystembackend/Application/Features/Materials/Commands/Create/CreateMaterialCommandValidator.cs
+++ librarymanagementsystem-backend/src/librarymanagementsystembackend/Application/Features/Materials/Commands/Create/CreateMaterialCommandValidator.cs
@@ -1,4 +1,5 @@
 using FluentValidation;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Application.Features.Materials.Commands.Create;
 
@@ -6,11 +7,16 @@ public class CreateMaterialCommandValidator : AbstractValidator<CreateMaterialCo
 {
     public CreateMaterialCommandValidator()
     {
-        RuleFor(c => c.Name).NotEmpty();
-        RuleFor(c => c.Description).NotEmpty();
-        RuleFor(c => c.PublicationDate).NotEmpty();
-        RuleFor(c => c.PunishmentAmount).NotEmpty();
-        RuleFor(c => c.IsBorrowable).NotEmpty();
-        RuleFor(c => c.BorrowDay).NotEmpty();
+        RuleFor(c => c.Name).NotEmpty().MinimumLength(1);
+
+        RuleFor(c => c.Description).MaximumLength(400)
+            .When(c => !string.IsNullOrEmpty(c.Description));
+
+        RuleFor(c => c.PublicationDate).NotEmpty().GreaterThan(DateTime.MinValue.Date)
+            .LessThanOrEqualTo(DateTime.Now.Date);
+
+        RuleFor(c => c.PunishmentAmount).NotEmpty().GreaterThanOrEqualTo(0);
+        RuleFor(c => c.IsBorrowable).Must(value => value == true || value == false);
+        RuleFor(c => c.BorrowDay).Must((c, borrowDay) => !c.IsBorrowable || borrowDay > 0);
     }
 }
