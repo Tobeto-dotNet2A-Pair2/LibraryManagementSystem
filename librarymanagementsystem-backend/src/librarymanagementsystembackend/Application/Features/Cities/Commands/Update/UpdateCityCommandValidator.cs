9b088ce181c2d61700405a65a276a63057e61285
yasarzeynep <zy_yasar@outlook.com>
2024-04-04 13:37:47 +0300

    add validation for city

--- librarymanagementsystem-backend/src/librarymanagementsystembackend/Application/Features/Cities/Commands/Update/UpdateCityCommandValidator.cs
+++ librarymanagementsystem-backend/src/librarymanagementsystembackend/Application/Features/Cities/Commands/Update/UpdateCityCommandValidator.cs
@@ -6,7 +6,8 @@ public class UpdateCityCommandValidator : AbstractValidator<UpdateCityCommand>
 {
     public UpdateCityCommandValidator()
     {
-        RuleFor(c => c.Id).NotEmpty();
-        RuleFor(c => c.CityName).NotEmpty();
+        RuleFor(c => c.Id).NotEmpty().Must(id => Guid.TryParse(id.ToString(), out _));
+        //.WithMessage("ID alaný geçersiz bir GUID formatýna sahip.");
+        RuleFor(c => c.CityName).NotEmpty().Length(2, 50).Matches("^[a-zA-ZðüþöçÐÜÞÝÖÇ ]+$");
     }
 }
