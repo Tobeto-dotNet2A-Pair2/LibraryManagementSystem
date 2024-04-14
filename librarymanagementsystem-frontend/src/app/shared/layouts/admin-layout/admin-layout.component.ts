3d0174a6f456d81e5b79bcd7878929b929f02942
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-04-14 23:09:43 +0300

    add aside component

--- librarymanagementsystem-frontend/src/app/shared/layouts/admin-layout/admin-layout.component.ts
+++ librarymanagementsystem-frontend/src/app/shared/layouts/admin-layout/admin-layout.component.ts
@@ -3,13 +3,14 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from '../../components/header/header.component';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { AsideComponent } from '../../components/aside/aside.component';
 
 
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [CommonModule, FormsModule, HeaderComponent, SidebarComponent],
+  imports: [CommonModule, FormsModule, HeaderComponent, SidebarComponent, AsideComponent],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.scss'
 })
