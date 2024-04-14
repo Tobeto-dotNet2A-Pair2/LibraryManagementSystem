79409a94cd94793717e79e2986bb88697b6b2f2f
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-04-14 20:44:54 +0300

    admin theme installed

--- librarymanagementsystem-frontend/src/app/shared/layouts/admin-layout/admin-layout.component.ts
+++ librarymanagementsystem-frontend/src/app/shared/layouts/admin-layout/admin-layout.component.ts
@@ -1,14 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.scss'
 })
 export class AdminLayoutComponent  {
+
+
 
 }
