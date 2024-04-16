3b32ff513a22d0d66b4380f795e2779eca7c7f45
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-04-16 14:31:47 +0300

    add toolbar component

--- librarymanagementsystem-frontend/src/app/shared/layouts/admin-layout/admin-layout.component.ts
+++ librarymanagementsystem-frontend/src/app/shared/layouts/admin-layout/admin-layout.component.ts
@@ -5,13 +5,14 @@ import { HeaderComponent } from '../../components/header/header.component';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { AsideComponent } from '../../components/aside/aside.component';
 import { RouterModule } from '@angular/router';
+import { ToolbarComponent } from '../../toolbar/toolbar.component';
 
 
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [CommonModule, FormsModule, HeaderComponent, SidebarComponent, AsideComponent,RouterModule],
+  imports: [CommonModule, FormsModule, HeaderComponent, SidebarComponent, AsideComponent,RouterModule, ToolbarComponent],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.scss'
 })
---
+++ librarymanagementsystem-frontend/src/app/shared/toolbar/toolbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-toolbar',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './toolbar.component.html',
+  styleUrl: './toolbar.component.scss'
+})
+export class ToolbarComponent {
+
+}
