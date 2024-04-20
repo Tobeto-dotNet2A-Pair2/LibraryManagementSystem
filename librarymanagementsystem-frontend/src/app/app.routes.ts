65588f2524c7ebcede0c2c5ecfac534c55d29a6c
aysekardas <aysekardas7@gmail.com>
2024-04-21 01:41:37 +0300

    branch routes

--- librarymanagementsystem-frontend/src/app/app.routes.ts
+++ librarymanagementsystem-frontend/src/app/app.routes.ts
@@ -4,9 +4,10 @@ import { authGuard } from './core/guards/auth.guard';
 import { AuthComponent } from './features/pages/auth/auth.component';
 import { AdminLayoutComponent } from './shared/layouts/admin-layout/admin-layout.component';
 import { DashboardComponent } from './features/pages/dashboard/dashboard.component';
-import { BranchComponent } from './features/pages/branch/branch.component';
 import { MaterialListComponent } from './features/pages/materials/material-list/material-list.component';
 import { MemberListComponent } from './features/pages/members/member-list/member-list.component';
+import { AddBranchFormComponent } from './features/pages/branches/add-branch-form/add-branch-form.component';
+import { BranchListComponent } from './features/pages/branches/branch-list/branch-list.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'adminpage', pathMatch: 'full' },
@@ -21,9 +22,10 @@ export const routes: Routes = [
     children: [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
       { path: 'dashboard', component: DashboardComponent },
-      {path: 'branches', component:BranchComponent},
       {path: 'material-lists', component:MaterialListComponent },
-      {path: 'members', component:MemberListComponent}
+      {path: 'members', component:MemberListComponent},
+      {path: 'add-branches', component:AddBranchFormComponent},
+      {path:'branch-lists', component:BranchListComponent},
     ]
   }
 ];
