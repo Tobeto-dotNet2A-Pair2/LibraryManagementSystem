ea68a3c1f2b5c60d720ff3aa55d2ef0d5a4bf632
aysekardas <aysekardas7@gmail.com>
2024-04-26 14:16:07 +0300

    add provider

--- librarymanagementsystem-frontend/src/app/app.config.ts
+++ librarymanagementsystem-frontend/src/app/app.config.ts
@@ -3,8 +3,31 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { getAppProviders } from './shared/providers/app-providers';
+import { AddressBaseService } from './features/services/abstracts/address-base.service';
+import { AddressService } from './features/services/concretes/address.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { MaterialBaseService } from './features/services/abstracts/material-base.service';
+import { MaterialService } from './features/services/concretes/material.service';
+import { MaterialCopyBaseService } from './features/services/abstracts/material-copy-base.service';
+import { MaterialCopyService } from './features/services/concretes/material-copy.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [getAppProviders()]
+  providers: [getAppProviders(),
+          provideRouter(routes),
+          HttpClientModule,
+          HttpClient,
+            {
+              provide:AddressBaseService,
+              useClass:AddressService,
+            },
+            {
+              provide:MaterialBaseService,
+              useClass: MaterialService,
+            },
+            {
+              provide:MaterialCopyBaseService,
+              useClass:MaterialCopyService,
+            }
+         ],
 };
-//test
+
