82e347e72db08448fe857efa8164561058ee10c3
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-04-29 16:22:13 +0300

    added member-profile

--- librarymanagementsystem-frontend/src/app/core/services/concretes/members.service.ts
+++ librarymanagementsystem-frontend/src/app/core/services/concretes/members.service.ts
@@ -63,4 +63,6 @@ export class MembersService extends MemberBaseService {
     )
   }
 
+  
+
 }
--- librarymanagementsystem-frontend/src/app/features/pages/members/member-profile/member-profile.component.ts
+++ librarymanagementsystem-frontend/src/app/features/pages/members/member-profile/member-profile.component.ts
@@ -1,16 +1,45 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { MembersService } from '../../../../core/services/concretes/members.service';
 import { ActivatedRoute } from '@angular/router';
+import { GetByIdMemberResponse } from '../../../models/responses/members/get-by-id-member-response';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { environment } from '../../../../../environments/environment';
 
 @Component({
   selector: 'app-member-profile',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './member-profile.component.html',
-  styleUrl: './member-profile.component.scss'
+  styleUrl: './member-profile.component.scss',
 })
-export class MemberProfileComponent  {
-  constructor(private membersService: MembersService, private activatedRoute:ActivatedRoute) {}
-  
+export class MemberProfileComponent implements OnInit {
+  memberId!: string;
+  memberByIdList: GetByIdMemberResponse[] = [];
+  constructor(private httpClient: HttpClient, private route: ActivatedRoute ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.memberId = params['id'];
+      console.log('Member ID:', this.memberId);
+    });
+
+    this.getMemberById();
+    
+  }
+
+  getMemberById() {
+    // backend'e istek atıp verileri çek
+    this.httpClient.get<GetByIdMemberResponse>(`${environment.API_URL}/Members/${this.memberId}`).subscribe({
+      next: (response: GetByIdMemberResponse) => {
+        console.log('Backendden cevap geldi:', response);
+        this.memberByIdList =[response];
+      },
+      error: (error) => {
+        console.log('Backendden hatalı cevap geldi:', error);
+      },
+      complete: () => {
+        console.log('Backend isteği sonlandı.');
+      },
+    });
+  }
 }
