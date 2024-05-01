39a9959976dce64244893f67bc858903bf17e587
Güven Çabat <gvncbt@gmail.com>
2024-05-01 17:22:42 +0300

    deleted custom-dropdown component

--- librarymanagementsystem-frontend/src/app/features/pages/materials/add-material-form/add-material-form.component.ts
+++ librarymanagementsystem-frontend/src/app/features/pages/materials/add-material-form/add-material-form.component.ts
@@ -134,45 +134,45 @@ export class AddMaterialFormComponent implements OnInit {
   }
 
   //Custom-Dropdown
-  @ViewChild('txtSearchValue') txtSearchValue!: ElementRef;
+  // @ViewChild('txtSearchValue') txtSearchValue!: ElementRef;
 
-  options = [
-    { value: '1', displayText: 'Option 1', hidden: false },
-    { value: '2', displayText: 'Option 2', hidden: false },
-    { value: '3', displayText: 'Option 3', hidden: false },
-  ];
+  // options = [
+  //   { value: '1', displayText: 'Option 1', hidden: false },
+  //   { value: '2', displayText: 'Option 2', hidden: false },
+  //   { value: '3', displayText: 'Option 3', hidden: false },
+  // ];
 
-  selectedOption = this.options[0];
-  isOpen = false;
+  // selectedOption = this.options[0];
+  // isOpen = false;
 
-  filterOptions(value: string): void {
-    this.options.forEach((option) => {
-      option.hidden =
-        option.displayText.toLowerCase().indexOf(value.toLowerCase()) === -1;
-    });
-  }
+  // filterOptions(value: string): void {
+  //   this.options.forEach((option) => {
+  //     option.hidden =
+  //       option.displayText.toLowerCase().indexOf(value.toLowerCase()) === -1;
+  //   });
+  // }
 
-  onSelect(option: any): void {
-    this.selectedOption = option;
-    this.isOpen = false;
-  }
+  // onSelect(option: any): void {
+  //   this.selectedOption = option;
+  //   this.isOpen = false;
+  // }
 
-  toggleDropdown(): void {
-    this.isOpen = !this.isOpen;
-    if (this.isOpen) {
-      setTimeout(() => this.txtSearchValue.nativeElement.focus(), 0);
-    }
-  }
+  // toggleDropdown(): void {
+  //   this.isOpen = !this.isOpen;
+  //   if (this.isOpen) {
+  //     setTimeout(() => this.txtSearchValue.nativeElement.focus(), 0);
+  //   }
+  // }
 
-  closeDropdown(event: MouseEvent): void {
-    if (!(event.target as HTMLElement).closest('.dropdown-select')) {
-      this.isOpen = false;
-    }
-  }
+  // closeDropdown(event: MouseEvent): void {
+  //   if (!(event.target as HTMLElement).closest('.dropdown-select')) {
+  //     this.isOpen = false;
+  //   }
+  // }
 
-  onKeyDown(event: KeyboardEvent): void {
-    if (event.key === 'Escape') {
-      this.isOpen = false;
-    }
-  }
+  // onKeyDown(event: KeyboardEvent): void {
+  //   if (event.key === 'Escape') {
+  //     this.isOpen = false;
+  //   }
+  // }
 }
