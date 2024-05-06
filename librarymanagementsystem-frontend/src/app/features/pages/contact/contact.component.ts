c26d8bdc6a9854f6bd3edc763269247b28037b8a
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-05-06 15:09:45 +0300

    add social components to the other components

--- librarymanagementsystem-frontend/src/app/features/pages/contact/contact.component.ts
+++ librarymanagementsystem-frontend/src/app/features/pages/contact/contact.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SocialsComponent } from '../../../shared/components/socials/socials.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [SocialsComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
--- librarymanagementsystem-frontend/src/app/shared/pages/homepage/homepage.component.ts
+++ librarymanagementsystem-frontend/src/app/shared/pages/homepage/homepage.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SearchComponent } from '../../components/search/search.component';
 import { HeroComponent } from '../../../features/components/hero/hero.component';
+import { SocialsComponent } from '../../components/socials/socials.component';
 
 @Component({
   standalone: true,
-  imports: [SearchComponent,  HeroComponent],
+  imports: [SearchComponent,  HeroComponent, SocialsComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss',
 })
