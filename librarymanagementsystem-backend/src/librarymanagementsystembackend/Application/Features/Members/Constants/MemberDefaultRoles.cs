972e9d868345f1945fef3b81dcf4993d296023e0
aysekardas <aysekardas7@gmail.com>
2024-05-10 00:51:27 +0300

    update user role

--- librarymanagementsystem-backend/src/librarymanagementsystembackend/Application/Features/Members/Constants/MemberDefaultRoles.cs
+++ librarymanagementsystem-backend/src/librarymanagementsystembackend/Application/Features/Members/Constants/MemberDefaultRoles.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace Application.Features.Members.Constants;
 public static class MemberDefaultRoles
 {
-    public static readonly string[] Roles = ["Auth.RevokeToken","Members.Read","Members.Create","Members.Update", "Addresses.Admin","Streets.Admin", "Materials.Admin"];
+    public static readonly string[] Roles = ["Auth.RevokeToken","Members.Read","Members.Create","Members.Update", "Addresses.Admin","Streets.Admin" ,"Materials.Admin", "BorrowedMaterials.Create","BorrowedMaterials.Read", "BorrowedMaterials.Write","BorrowedMaterials.Update", "MaterialCopies.Read", "MaterialCopies.Write", "MaterialCopies.Create", "MaterialCopies.Update", "Locations.Admin", "MaterialGenres.Admin", "MaterialProperties.Admin", "MaterialTypes.Admin"];
 
   
 }
