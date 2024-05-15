5a8c9e00fa8892dd35bec8a793c52de6d31820fe
Zeynep Yaşar <zynpsema@gmail.com>
2024-05-16 00:34:14 +0300

    add loading service and interceptor

---
+++ librarymanagementsystem-frontend/src/app/core/services/concretes/loading.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoadingService {
+
+  isLoading: boolean = false;
+  constructor() {}
+
+  setLoading(value: boolean) {
+    this.isLoading = value;
+  }
+}
